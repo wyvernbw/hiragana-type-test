cbafda7bd199ecd6f4250458e209e7e3afa2a762
wyvernbw <iamwyvern511@gmail.com>
2025-03-06 18:30:03 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Button } from './components/ui/button';
+
+export const App = () => {
+	return (
+		<div className="w-screen min-h-screen flex flex-col items-center justify-center"></div>
+	);
+};
